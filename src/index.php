0cda9c1f77c319755e5217cb351547d292810f2c
takahara tomoaki <jihyun8286@gmail.com>
2022-09-15 10:58:38 +0900

    phpにした

--- src/index.php
+++ src/index.php
@@ -11,132 +11,21 @@
 </head>
 
 <body>
-    <header>
-        <div class="headerInner">
-            <h1>ガチで東京の人しか解けない！ #東京の難読地名クイズ</h1>
-        </div>
-    </header>
-    <main>
-        <div class="mainInner">
-            <article>
-                <h2>1. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; こうわ</b></li>
-                    <li class="choices"><b> &emsp; たかわ</b></li>
-                    <li class="choices"><b> &emsp; たかなわ</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>2. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; かめいど</b></li>
-                    <li class="choices"><b> &emsp; かめと</b></li>
-                    <li class="choices"><b> &emsp; かめど</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>3. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; こうじまち</b></li>
-                    <li class="choices"><b> &emsp; おかとまち</b></li>
-                    <li class="choices"><b> &emsp; かゆまち</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>4. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; ごせいもん</b></li>
-                    <li class="choices"><b> &emsp; おかどもん</b></li>
-                    <li class="choices"><b> &emsp; おなりもん</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>5. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/6a235aaa10f0bd3ca57871f76907797b.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; たたら</b></li>
-                    <li class="choices"><b> &emsp; たたりき</b></li>
-                    <li class="choices"><b> &emsp; とどろき</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>6. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/0b6789cf496fb75191edf1e3a6e05039.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; しゃくじい</b></li>
-                    <li class="choices"><b> &emsp; せきこうい</b></li>
-                    <li class="choices"><b> &emsp; いじい</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>7. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/23e698eec548ff20a4f7969ca8823c53.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; ざっしき</b></li>
-                    <li class="choices"><b> &emsp; ざっしょく</b></li>
-                    <li class="choices"><b> &emsp; ぞうしき</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>8. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; おかちまち</b></li>
-                    <li class="choices"><b> &emsp; ごしろちょう</b></li>
-                    <li class="choices"><b> &emsp; みとちょう</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>9. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; ししぼね</b></li>
-                    <li class="choices"><b> &emsp; ろっこつ</b></li>
-                    <li class="choices"><b> &emsp; しこね</b></li>
-                </ul>
-            </article>
-            <article>
-                <h2>10. この地名はなんて読む？</h2>
-                <img src="https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png" alt="たかなわ">
-                <ul>
-                    <li class="choices"><b> &emsp; こぐれ</b></li>
-                    <li class="choices"><b> &emsp; こばく</b></li>
-                    <li class="choices"><b> &emsp; こしゃく</b></li>
-                </ul>
-            </article>
-            </div>
-    </main>
-
-
-
-
-
-
-
-
-
-
-
-
-
-</body>
-
     <header class="header">
         <ul class="header_list_left">
-            <li id="headerListItemHumburger" class="header_list_item_humburger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"></path><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path></svg></li>
+            <li id="headerListItemHumburger" class="header_list_item_humburger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M0 0h24v24H0z" fill="none"></path>
+                    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+                </svg></li>
             <li class="header_list_item_kuizy"><a href="https://kuizy.net/">kuizy</a></li>
         </ul>
         <ul class="header_list_right">
             <li class="header_list_item_create"><a href="https://kuizy.net/prepare">クイズ・診断を作る</a></li>
             <li class="header_list_item_search"><a href="https://kuizy.net/search">
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 20 20">
-                    <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                    <path d="M0 0h24v24H0z" fill="none"></path>
-                </svg>検索</a>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" viewBox="0 0 20 20">
+                        <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                    </svg>検索</a>
             </li>
         </ul>
     </header>
@@ -162,14 +51,14 @@
     <main class="main">
         <h1 class="title">ガチで東京の人しか解けない！ #東京の難読地名クイズ</h1>
         <section class="quiz">
-        <!-- beforebegin -->
-        <div id="question">
-            <!-- afterbegin -->
-            <!-- beforeend -->
-        </div>
-        <!-- afterend -->
-        
-        
+            <!-- beforebegin -->
+            <div id="question">
+                <!-- afterbegin -->
+                <!-- beforeend -->
+            </div>
+            <!-- afterend -->
+
+
             <script src="kuizy.js"></script>
         </section>
     </main>
@@ -193,4 +82,5 @@
         <p class="footer_small"><small>&copy; 2021 Nooon LLC</small></p>
     </footer>
 </body>
+
 </html>
